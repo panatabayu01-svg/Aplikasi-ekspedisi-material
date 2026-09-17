5c25f6c081cef3759d86f50a610962a91b170bba
panatabayu01-svg <panata.bayu01@gmail.com>
2026-09-18 02:24:44 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'ekspedisi-material-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './favicon-32.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(APP_FILES);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names
+          .filter(function(name) {
+            return name !== CACHE_NAME;
+          })
+          .map(function(name) {
+            return caches.delete(name);
+          })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .catch(function() {
+        return caches.match(event.request);
+      })
+  );
+});
